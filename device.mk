d2c700a83618781ed367d723d94de753e7f2320d
Surdu Petru <surdu_petru@yahoo.com>
2016-11-20 09:47:56 +0100

    build new HWC

--- device.mk
+++ device.mk
@@ -178,6 +178,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=0 \
     persist.sys.root_access=1
 
+# HWC
+PRODUCT_PACKAGES += \
+    hwcomposer.hi3650
+
 # Power HAL
 PRODUCT_PACKAGES += \
     power.hi3650
