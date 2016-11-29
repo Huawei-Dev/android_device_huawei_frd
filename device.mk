561bd11a75822f41e90a7e10fe2a7be531b865ad
Surdu Petru <surdu_petru@yahoo.com>
2016-11-29 21:29:13 +0100

    revert codecs to google_video instead google_video_le

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Audio configuration file
 PRODUCT_COPY_FILES += \
